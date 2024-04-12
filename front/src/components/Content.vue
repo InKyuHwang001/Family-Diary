836829b55e3f5d6029cbea6612fbafaf3ce18d45
InKyuHwang001 <hwanginkyu42@naver.com>
2024-04-12 17:28:08 +0900

    intit: frontend
    버젼 정보
    - node 18
    - vue : 3.4.21

---
+++ front/src/components/Content.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="greetings">
+    This is What I want
+  </div>
+</template>
+
+<style scoped>
+
+.header {
+  padding: 0;
+  height: 60px;
+}
+</style>
---
+++ front/src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="greetings">
+    asdadasdsa
+  </div>
+</template>
+
+<style scoped>
+
+.header {
+  padding: 0;
+  height: 60px;
+}
+</style>
